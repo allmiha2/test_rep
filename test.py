0cd8fb3feb0eee4c1ff794a996c55ce00107b031
allmiha2 <44604300+allmiha2@users.noreply.github.com>
2022-05-01 22:42:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print("Buna ziua there dear sir Lewis Hamilton. Viva mexico cabroneeeees")
 
 print('grazzie ragazzi forza craiova main')
 
-print('eu cred ca e asa')
+print('eu nu cred ca e asa')
 
 
 print('forta craiovaa')
